f3bbf2b95cc3ea79237676457644ba2c1a9d8c3b
Jonathanrrs <yoni_12rrs@hotmail.com>
2023-01-12 19:47:33 -0600

    Servir archivos de manera controlada

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -2,10 +2,14 @@ import {
   BadRequestException,
   Controller,
   Post,
+  Get,
   UploadedFile,
+  Param,
+  Res,
+  UseInterceptors,
 } from '@nestjs/common';
-import { UseInterceptors } from '@nestjs/common/decorators';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 import { fileFilter, fileNamer } from './helpers';
@@ -13,6 +17,16 @@ import { fileFilter, fileNamer } from './helpers';
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('/product/:imageName')
+  findProductImage(
+    /*  esto rompe la funcionalidad de nest de la respuesta, para manualmente emitir mi respuesta, se saltan interceptores de nest, entre otras funcionalidades  */
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(
@@ -30,6 +44,7 @@ export class FilesController {
     if (!file) {
       throw new BadRequestException('Make sure that the file is an image');
     }
-    return { fileName: file.originalname };
+    const secureUrl = `${file.filename}`;
+    return { secureUrl };
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { join } from 'path';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { existsSync } from 'fs';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  getStaticProductImage(imageName: string) {
+    const path = join(__dirname, '../../static/products', imageName);
+
+    if (!existsSync) {
+      throw new NotFoundException(`No product found with image ${imageName}`);
+    }
+    return path;
+  }
+}
