42bcb8e3a2ae867f009e74f36325f9d4251ee5b3
Jonathanrrs <yoni_12rrs@hotmail.com>
2023-01-18 12:04:03 -0600

    Documentar DTO

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,12 +1,22 @@
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PaginationDto {
+  @ApiProperty({
+    default: 10,
+    description: 'How many rows do you need?',
+  })
   @IsOptional()
   @IsPositive()
   /* transformar data */
   @Type(() => Number) /* esto es otra manera a enableimplicitconversion: true */
   limit?: number;
+
+  @ApiProperty({
+    default: 0,
+    description: 'How many rows do you want to skip?',
+  })
   @IsOptional()
   @Type(() => Number) /* esto es otra manera a enableimplicitconversion: true */
   @Min(0)
--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+// import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateProductDto } from './create-product.dto';
 
+/* se hace la importacion de PartialType desde swagger, para eso extender lo que tiene el createProductDto, sin ella la importacion normal no lo toma */
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
